cb7085d2707da23f716090c5d21c64e2a0351fdb
Salome Ghaghanidze <sghaghanidze@bog.ge>
2022-07-29 16:39:37 +0400

    first

---
+++ list.js
@@ -0,0 +1,131 @@
+
+
+import {listArray , button , ul ,  total }  from './script.js' ; 
+
+
+button.onclick = () =>
+{
+
+    addGroceryListEl(document.getElementById('todoinput').value);
+}
+
+
+function delEl(e)  
+{
+   
+let parentId = document.getElementById(e.target.id).parentNode.id;
+console.log(parentId);
+      for (let i =0 ; i< listArray.length ; i++)   {
+        if(listArray[i].id == parentId)   {
+      ul.removeChild(document.getElementById(parentId)) ;
+      listArray.splice(i,1) ;
+      total.innerHTML=`Total Items : ${listArray.length}`
+        }
+     
+} } 
+
+
+function doneBtn(e)
+
+{  
+
+    let parentId = document.getElementById(e.target.id).parentNode;
+
+    let myEl= document.getElementById(e.target.id);
+    
+    if (myEl.getAttribute('data-is-done') == 'N') {
+        parentId.style.textDecoration='line-through' ;
+        myEl.setAttribute('data-is-done','Y');
+      }
+
+      else {   
+         parentId.style.textDecoration='none' ;
+         myEl.setAttribute('data-is-done','N');
+    }
+
+}
+
+
+
+
+
+
+class listObject 
+{
+    constructor (whatToBuy , id)
+    {
+       this.whatToBuy=whatToBuy;
+       this.id=id;
+
+    }
+   
+    createItem ()
+    {
+          let el =  document.createElement('li');
+          el.setAttribute("id",this.id) ;
+          el.innerText=this.whatToBuy;
+          let root =  document.getElementById('cont-list');
+          root.appendChild(el);
+          return el
+    }
+}
+
+
+
+class bttnObject 
+{
+    constructor (id)
+    {
+       this.id=id;
+    }
+    createBttn ()
+    {
+          let el =  document.createElement('button');
+          el.setAttribute("id",this.id+2);
+          el.className='del-btn' ; 
+          el.innerHTML='Delete';
+          el.setAttribute("type","button")
+          el.addEventListener("click",delEl)
+
+          return el
+    }
+
+
+    createPnt ()
+    {
+          let el =  document.createElement('button');
+          el.setAttribute("id",this.id+1);
+          el.className='done-btn' ; 
+          el.innerHTML='Done';
+          el.setAttribute("type","button");
+          el.setAttribute('data-is-done','N');
+          el.addEventListener("click",doneBtn);
+
+          return el
+    }
+
+}
+
+
+
+async function addGroceryListEl (element) 
+{
+
+    let l = await new listObject (element, Date.now());
+    let p = await l.createItem(); 
+    console.log(p);
+    let btn = new bttnObject (Date.now()); 
+    let bt= btn.createBttn();
+    let edit= btn.createPnt();
+    console.log(edit)
+    p.appendChild(bt); 
+    p.appendChild(edit);
+
+  
+    listArray.push(p); 
+    total.innerHTML=`Total Items : ${listArray.length}`; 
+
+};
+
+
+
---
+++ script.js
@@ -0,0 +1,7 @@
+ export let listArray= []; 
+ export let button = document.getElementById("plus") ;
+ export let ul = document.getElementById('cont-list'); 
+ export let total = document.getElementById('total');
+
+
+
